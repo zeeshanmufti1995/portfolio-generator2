9ee02d6c34917f038319e975622595a462dd3060
Zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-06-30 08:15:03 -0500

    generate HTML file

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log()
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
